4150f15b10b560b781276bcec0aee4a1ceb27328
Rafael <rafaelaugustogui@gmail.com>
2024-03-11 23:50:19 -0300

    atualização drastica

--- app.js
+++ app.js
@@ -1,211 +1,174 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-const salas = {};
-const sala_atual = {};
+const io = socketio(server);
 
-const gerar_id_aleatorio = () => {
-    return Math.random().toString(36).substr(2, 6);
-};
-
-const criar_sala = () => {
-    let id_sala = gerar_id_aleatorio();
-    while (salas[id_sala]) {
-        id_sala = gerar_id_aleatorio();
-    }
-    salas[id_sala] = {
-        usuarios: {},
-        dono: null,
-        temas: [],
-        rodada_iniciada: false
-    };
-    return id_sala;
-};
-
-const iniciar_rodada = (id_sala) => {
-    if (salas[id_sala].rodada_iniciada) {
-        return;
-    }
-    salas[id_sala].rodada_iniciada = true;
-    let tempo_restante = 15; 
-    const timer_interval = setInterval(() => {
-        if (tempo_restante <= 0 || salas[id_sala].temas.length == Object.keys(salas[id_sala].usuarios).length) {
-            clearInterval(timer_interval);
-            tempo_esgotado(id_sala);
-            return;
-        }
-        io.to(id_sala).emit('atualizar_tempo', tempo_restante);
-        tempo_restante--;
-    }, 1000);
-    io.to(id_sala).emit('rodada_iniciada', tempo_restante);
-};
-
-const enviar_tema = (id_sala, id_usuario, tema) => {
-    if (!salas[id_sala].rodada_iniciada) {
-        return;
-    }
-    if (salas[id_sala].temas.some(t => t.id_usuario === id_usuario)) {
-        return; 
-    }
-    salas[id_sala].temas.push({ id_usuario, tema });
-};
-
-const tempo_esgotado = (id_sala) => {
-    const usuarios_ids = Object.keys(salas[id_sala].usuarios);
-    const indice_sorteado = Math.floor(Math.random() * usuarios_ids.length);
-    const usuario_sorteado = salas[id_sala].usuarios[usuarios_ids[indice_sorteado]];
-
-    io.to(usuario_sorteado.id).emit('camaleao');
-
-    const outros_temas = salas[id_sala].temas.filter(tema => tema.id_usuario !== usuario_sorteado.id);
-    const outros_usuarios = usuarios_ids.filter(id => id !== usuario_sorteado.id);
-    if (outros_temas.length > 0) {
-        const tema_rodada = outros_temas[Math.floor(Math.random() * outros_temas.length)];
-        
-        outros_usuarios.forEach(id => {
-            const usuario = salas[id_sala].usuarios[id];
-            io.to(usuario.id).emit('tema_rodada', tema_rodada.tema);
-        });
-    }
-};
-
+salas = {}
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-io.on('connect', (socket) => {
-    socket.on('criar_sala', () => {
-        if (sala_atual[socket.id]) {
-            socket.emit('erro', 'Você já está em uma sala. Por favor, saia dela antes de criar outra.');
+io.on('connection', socket => {
+    socket.on('entrar_sala', (data) => { 
+        if (salas[data.id_sala] == undefined) {
+            socket.emit('erro', 'Essa sala não existe!');
             return;
         }
-
-        const id_sala = criar_sala();
-        socket.emit('sala_criada', id_sala);
-    });
-
-    socket.on('entrar_sala', ({ id_sala, nome_usuario }) => {
-        if (sala_atual[socket.id] != null) {
-            socket.emit('erro', 'Você já está em uma sala. Por favor, saia dela antes de entrar em outra.');
-            return;
-        }
-        if (!salas[id_sala]) {
-            socket.emit('erro', 'Sala não encontrada. Por favor, verifique o ID da sala.');
-            return;
-        }
-        entrar_sala(socket, id_sala, nome_usuario);
-        socket.emit('sala_entrar', salas[id_sala]);
-    });
-
-    socket.on('sair_sala', () => {
-        sair_sala(socket);
-        socket.emit('sala_sair');
-    });
     
-    socket.on('listar_salas', () => {
-        listar_salas();
-    });
+        const nomeUsuarioExistente = salas[data.id_sala].pessoas.find(pessoa => pessoa.nome.toLowerCase() === data.nome_usuario.toLowerCase());
+        if (nomeUsuarioExistente) {
+            socket.emit('erro', 'Esse nome de usuário já está em uso na sala!');
+            return;
+        }
     
+        let usuario = { id: socket.id, nome: data.nome_usuario };
+        if (salas[data.id_sala].pessoas.length === 0) {
+            salas[data.id_sala].dono = usuario;
+        }
+        salas[data.id_sala].pessoas.push(usuario);
+        socket.join(data.id_sala);
+        atualizarListaPessoasNaSala(data.id_sala);
+      
+    });
+
     socket.on('disconnect', () => {
-        if (sala_atual[socket.id]) {
-            sair_sala(socket, sala_atual[socket.id]);
-        }
-    });
-
-    socket.on('iniciar_rodada', (id_sala) => {
-        iniciar_rodada(id_sala);
-    });
-
-    socket.on('enviar_tema', (tema) => {
-        enviar_tema(sala_atual[socket.id], socket.id, tema);
-    });
-
-    const entrar_sala = (socket, id_sala, nome_usuario) => {
-        if (!nome_usuario) {
-            socket.emit('erro', 'É necessário definir um nome de usuário.');
-            return;
-        }
-        if (!salas[id_sala]) {
-            socket.emit('erro', 'Sala não encontrada. Por favor, verifique o ID da sala.');
-            return;
-        }
-    
-        const usuariosSala = Object.values(salas[id_sala].usuarios);
-        const usuarioExistente = usuariosSala.find(usuario => usuario.nome.toLowerCase() === nome_usuario.toLowerCase());
-        if (usuarioExistente) {
-            socket.emit('erro', 'Já existe um usuário com o mesmo nome nesta sala. Por favor, escolha outro nome.');
-            return;
-        }
-    
-        socket.join(id_sala);
-        const dono = Object.keys(salas[id_sala].usuarios).length === 0 || !salas[id_sala].usuarios;
-        if (dono)
-            salas[id_sala].dono = {id: socket.id, nome: nome_usuario};
-    
-        salas[id_sala].usuarios[socket.id] = { id: socket.id, nome: nome_usuario, dono };
-        sala_atual[socket.id] = id_sala;
-        listar_salas();
-        atualizar_lista_usuarios(id_sala);
-    };
-
-    
-    const sair_sala = (socket) => {
-        if (!sala_atual[socket.id]) {
-            return; 
-        }
-    
-        const id_sala = sala_atual[socket.id];
-        
-        if (!salas[id_sala] || !salas[id_sala].usuarios) {
-            return; 
-        }
-    
-        delete salas[id_sala].usuarios[socket.id];
-        
-        if (Object.keys(salas[id_sala].usuarios).length === 0) {
-            delete salas[id_sala];
-        } else {
-            atualizar_dono(id_sala);
-            atualizar_lista_usuarios(id_sala);
-        }
-        listar_salas();
-        sala_atual[socket.id] = null; 
-        socket.leave(id_sala);
-    };
-
-    const listar_salas = () => {
-        const salas_disponiveis = Object.keys(salas).map(id => ({ id, dono: salas[id].dono }));
-        io.emit('salas_disponiveis', salas_disponiveis);
-    };
-    
-    
-    const atualizar_lista_usuarios = (id_sala) => {
-        if (salas[id_sala]) {
-            const usuarios = Object.values(salas[id_sala].usuarios);
-            io.to(id_sala).emit('lista_usuarios', usuarios);
-        }
-    };
-
-    const atualizar_dono = (id_sala) => {
-        if (salas[id_sala]) {
-            const usuarios_ids = Object.keys(salas[id_sala].usuarios);
-            if (!usuarios_ids.includes(salas[id_sala].dono)) {
-                const novo_dono_id = usuarios_ids[0];
-                salas[id_sala].dono = {id: novo_dono_id, nome: salas[id_sala].usuarios[novo_dono_id].nome};
-                io.to(id_sala).emit('novo_dono', novo_dono_id);
-                try {
-                    salas[id_sala].usuarios[novo_dono_id].dono = true;
-                } catch { }
+        for (const [sala_id, sala] of Object.entries(salas)) {
+            const index = sala.pessoas.findIndex(user => user.id === socket.id);
+            if (index !== -1) {
+                sala.pessoas.splice(index, 1);
+                if (sala.dono && sala.dono.id === socket.id) {
+                    if (sala.pessoas.length > 0)
+                        sala.dono = sala.pessoas[0]
+                    else
+                        delete salas[sala_id];
+                }
+                socket.leave(sala_id);
+                atualizarListaPessoasNaSala(sala_id);
             }
         }
-    };
+       
+    });
+    socket.on('iniciar_rodada', () => {
+        
+        const sala_id = sala_usuario(socket.id)
+       
+        if (sala_id != null && salas[sala_id].rodada == false)
+            rodada(sala_id);
+    });
+    socket.on('temas', (valores) => {
+        const sala_id = sala_usuario(socket.id);
+
+        if (!salas[sala_id] || !salas[sala_id].rodada) {
+            return;
+        }
+
+        if (salas[sala_id].temas && salas[sala_id].temas.some(t => t.usuario_id === socket.id)) {
+            return;
+        }
+    
+        if (!salas[sala_id].temas) {
+            salas[sala_id].temas = [];
+        }
+        salas[sala_id].temas.push({ usuario_id: socket.id, tema: valores.tema });
+        
+    });
 });
 
+function rodada(sala_id){
+    salas[sala_id].rodada = true;
+    let tempo_restante = 30;
+    const timer_interval = setInterval(() => {
+        if (salas[sala_id] == undefined) return;
+        if (tempo_restante <= 0 || salas[sala_id].temas.length == salas[sala_id].pessoas.length) {
+            
+            clearInterval(timer_interval);
+            finalizar_rodada(sala_id);
+            return;
+        }
+        io.to(sala_id).emit('rodada', {estado: 'entrada', tempo: tempo_restante});
+        tempo_restante--;
+    }, 1000);
+    io.to(sala_id).emit('rodada', {estado: 'iniciado' });
+}
+
+function finalizar_rodada(sala_id) {
+    const sala = salas[sala_id];
+    const pessoas = sala.pessoas;
+    const indice_sorteado = Math.floor(Math.random() * pessoas.length);
+    const usuario_sorteado = pessoas[indice_sorteado];
+
+    io.to(usuario_sorteado.id).emit('rodada', { estado: 'jogo', tema: '', camaleao: true});
+    
+    const outros_temas = sala.temas; 
+    let indice_tema = Math.floor(Math.random() * outros_temas.length);
+    while(outros_temas[indice_tema].usuario_id == usuario_sorteado.id){
+        indice_tema = Math.floor(Math.random() * outros_temas.length);
+    }
+    pessoas.forEach(usuario => {
+        if (usuario.id !== usuario_sorteado.id) {
+            io.to(usuario.id).emit('rodada', { estado: 'jogo', tema: outros_temas[indice_tema].tema, camaleao: false });
+        }
+    });
+    salas[id_sala].rodada = false;
+    salas[id_sala].temas = [];
+}
+
+
+function sala_usuario(id){
+    for (const [sala_id, sala] of Object.entries(salas)) {
+        const usuarioNaSala = sala.pessoas.find(usuario => usuario.id === id);
+        if (usuarioNaSala) {
+            return sala_id;
+        }
+    }
+    return null; 
+} 
+function atualizarListaPessoasNaSala(sala_id) {
+    if (!salas[sala_id]) {
+        return;
+    }
+    io.to(sala_id).emit('atualizar_pessoas_sala', salas[sala_id]);
+}
+
+function gerar_id() {
+    return Math.random().toString(36).substr(2, 6);
+}
+
+const criar_sala = (nome) => {
+    id_sala = gerar_id()
+    while(salas[id_sala] != undefined)
+        id_sala = gerar_id()
+    salas[id_sala] = {nome: `Sala de ${nome}`, dono: undefined, rodada: false, pessoas: [], temas: []}
+    return id_sala
+};
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/criar-sala', (req, res) => {
+    const nome = req.body.nome;
+    const sala_id = criar_sala(nome); 
+    res.redirect(`/sala/${sala_id}/${nome}`);
+});
+
+
+app.post('/entrar-sala', (req, res) => {
+    const nome = req.body.nome;
+    const sala_id = req.body.sala_id;
+    res.redirect(`/sala/${sala_id}/${nome}`);
+});
+
+app.get('/sala/:sala_id/:nome', (req, res) => {
+    const nome = req.params.nome; 
+    const sala_id = req.params.sala_id;
+    res.render('sala', { nome: nome, sala_id: sala_id });
+});
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log(`Servidor está rodando na porta ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Servidor rodando em ${PORT}`));
+ 
